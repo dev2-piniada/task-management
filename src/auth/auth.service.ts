910e27519fa152b19912d7cc507a5870973fbcde
Saneera Yapa <saneera.yapa@nuevotek.com>
2025-02-11 06:51:30 +1030

    Initial management

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { User } from './user.entity'
+import { UserRepository } from './user.respository'
+import { AuthCredentialsDto } from './dto/auth-credentials'
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: UserRepository,
+  ) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto
+
+    const user = this.userRepository.create({ username, password })
+    await this.userRepository.save(user)
+  }
+}
